081753bc14e4e121cf0d746fa0624bbf62199d82
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-03-20 20:44:32 +0100

    Draft: Remove `cmake` dependency

--- build.rs
+++ build.rs
@@ -1,20 +1,82 @@
 #![deny(warnings)]
 
-extern crate cmake;
+use std::path::{Path, PathBuf};
 
-use std::env;
+const INCLUDE_DIRS: [&str; 7] = [
+    "src/fake-zlib",
+    "src/deps/ots/include",
+    "src/deps/lz4/lib",
+    "src/deps/brotli/c/dec",
+    "src/deps/brotli/c/include",
+    "src/deps/woff2/include",
+    "src/deps/woff2/src",
+];
+
+fn build_lz4() {
+    cc::Build::new()
+        .file("src/deps/lz4/lib/lz4.c")
+        .includes(INCLUDE_DIRS)
+        .compile("lz4");
+}
+
+fn build_ots() {
+    let ots_sources = glob::glob("src/deps/ots/src/*.cc")
+        .expect("Invalid glob pattern")
+        .collect::<Result<Vec<PathBuf>, _>>()
+        .expect("vendored ots sources not found");
+
+    cc::Build::new()
+        .cpp(true)
+        .files(ots_sources)
+        .include("src/deps/ots/include")
+        .include("src/deps/lz4/lib")
+        .includes(INCLUDE_DIRS)
+        .std("c++11")
+        .compile("ots");
+}
+
+fn build_brotli() {
+    let brotli_sources = glob::glob("src/deps/brotli/c/**/*.c")
+        .expect("Invalid glob pattern")
+        .collect::<Result<Vec<PathBuf>, _>>()
+        .expect("vendored brotli sources not found");
+
+    cc::Build::new()
+        .files(brotli_sources)
+        .includes(INCLUDE_DIRS)
+        .compile("brotli");
+}
+
+fn build_woff2() {
+    let woff2_dir = Path::new("src/deps/woff2/src");
+    let file_names = ["table_tags.cc",
+    "variable_length.cc",
+    "woff2_common.cc",
+    "woff2_dec.cc",
+    "woff2_out.cc"];
+    let woff2_sources = file_names.iter().map(|name| woff2_dir.join(name));
+
+    cc::Build::new()
+        .files(woff2_sources)
+        .includes(INCLUDE_DIRS)
+        .std("c++11")
+        .warnings(false)
+        .compile("woff2");
+}
+
+fn build_ots_glue() {
+    cc::Build::new()
+        .file("src/ots_glue.cc")
+        .includes(INCLUDE_DIRS)
+        .std("c++11")
+        .compile("ots_glue");
+}
+
 
 fn main() {
-    let dst = cmake::Config::new("src").build();
-
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static=ots");
-    println!("cargo:rustc-link-lib=static=woff2");
-    println!("cargo:rustc-link-lib=static=brotli");
-    let target = env::var("TARGET").unwrap();
-    if target.contains("apple") {
-        println!("cargo:rustc-link-lib=c++");
-    } else if !target.contains("msvc") {
-        println!("cargo:rustc-link-lib=stdc++");
-    }
+    build_lz4();
+    build_ots();
+    build_brotli();
+    build_woff2();
+    build_ots_glue();
 }
